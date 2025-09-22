c6472dc8fecce2dbd47dcc038cb34373279990d5
ERROR404 <148620003+Asfaulla786@users.noreply.github.com>
2025-09-23 02:08:22 +0530

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", ))
-    API_HASH = os.getenv("API_HASH", '')
-    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
+    API_ID = int(os.getenv("API_ID","21519702" ))
+    API_HASH = os.getenv("API_HASH", '20fcf051ad48130f35fe01e82f5417cd')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '7976010249:AAHX1GvKqNOuUq2QxOBqgOtzYs7yabD6l00')
